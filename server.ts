a148208e5de330f153bc8c1a62cb41f0fce330ea
SakshiSukheja <87907995+SakshiSukheja@users.noreply.github.com>
2026-07-28 12:51:37 +0530

    Fix import path for ddtGenerator module

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import { createServer as createViteServer } from 'vite';
 import dotenv from 'dotenv';
-import { processDdtSummaryRequest } from './src/server/ddtGenerator.js';
+import { processDdtSummaryRequest } from './src/server/ddtGenerator';
 
 dotenv.config();
 
